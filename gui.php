e6904158b4b2dfc78aaae379824867407e7344e6
Mikayla Morrison <115184215+mikaylamorrison@users.noreply.github.com>
2024-11-21 15:12:32 -0500

    error display

--- gui.php
+++ gui.php
@@ -1,4 +1,9 @@
 <?php
+// error display
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Database connection settings
 $username = 'rpmanoha';
 $password = '04309408';
