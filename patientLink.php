d47b94c84fa3f3d27b8acccd14912973023ef3a3
Eric Wong <erickj.wong@gmail.com>
2024-11-21 18:44:07 -0500

    Update patientLink.php

--- patientLink.php
+++ patientLink.php
@@ -1,20 +1,14 @@
 <?php
-    // Copied from rubric hints
-    // Error display
     error_reporting(E_ALL);
     ini_set('display_errors', 'On');
-
     // Create connection to Oracle
-    $conn = oci_connect('rpmanoha', '04309408','(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNEC
-    T_DATA=(SID=orcl)))');
-
-    // checking for successful connection
+    $conn = oci_connect('rpmanoha', '04309408','(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))');
     if (!$conn) {
-    $m = oci_error();
-    echo $m['message'];
+        $m = oci_error();
+        echo $m['message'];
     }
-    else {
-    echo "successfully connected with oracle database";
+    else{
+        echo "successfully connected with oracle database";
     }
 ?>
 
