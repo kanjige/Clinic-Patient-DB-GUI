aa29f43500a71c5e7f2b34950de6666e0b4ff092
Mikayla Morrison <115184215+mikaylamorrison@users.noreply.github.com>
2024-11-21 15:33:25 -0500

    updated code

--- patientLink.php
+++ patientLink.php
@@ -1,39 +1,28 @@
 <?php
-// error display
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-// Database connection settings
-$username = 'rpmanoha';
-$password = '04309408';
-$connection_string = '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))';
-
-// Establishing connection
-$conn = oci_connect($username, $password, $connection_string);
-
-// Check if the connection was successful
-if (!$conn) {
-    $error = oci_error();
-    echo "Connection failed: " . $error['message'];
-    exit;
-} else {
-    echo "Connection successful!";
-}
-
-// from sample sheet
-$query = "Select *From test" ;
-$stid = oci_parse($conn, $query);
-$r = oci_execute($stid);
-if($r){
- // Fetch each row in an associative array
+    error_reporting(E_ALL);
+    ini_set('display_errors', 'On');
+    // Create connection to Oracle
+    $conn = oci_connect('rpmanoha', '04309408','(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNEC
+    T_DATA=(SID=orcl)))');
+    if (!$conn) {
+    $m = oci_error();
+    echo $m['message'];
+    }
+    else{
+    echo "successfully connected with oracle database";
+    }
+    $query = "Select *From test" ;
+    $stid = oci_parse($conn, $query);
+    $r = oci_execute($stid);
+    if($r){
+    // Fetch each row in an associative array
     while ($row = oci_fetch_array($stid, OCI_RETURN_NULLS+OCI_ASSOC)) {
     foreach ($row as $item) {
     echo $item," ";
     }
     echo "<br/>";
     }
-}
+    }
 ?>
 
 <!DOCTYPE html>
