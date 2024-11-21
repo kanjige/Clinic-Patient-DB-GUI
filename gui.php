43e4e7797093a112bd35d1f1deca7e698a4be3af
Mikayla Morrison <115184215+mikaylamorrison@users.noreply.github.com>
2024-11-21 14:44:09 -0500

    Added CSS and PHP Files

--- gui.php
+++ gui.php
@@ -1,1 +1,30 @@
+<?php 
 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="gui.css">
+    <link href="https://fonts.googleapis.com/css2?family=Assistant:wght@200..800&display=swap" rel="stylesheet">
+    <meta charset="utf-8">
+    <title>CPS510: Patient Clinic DB</title>
+</head>
+
+<body>
+    <header>
+        <h1>
+            Patient Clinic Database
+        </h1>
+    </header>
+
+    <main>
+
+       <section>
+            <h3>idk</h3>
+        </section>
+        <hr>
+
+    </main>
+</body>
+</html>
