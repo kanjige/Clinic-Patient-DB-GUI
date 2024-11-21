e67aaa74672ba552eb2aab908799b9fecb0a9ee6
Mikayla Morrison <115184215+mikaylamorrison@users.noreply.github.com>
2024-11-21 14:54:21 -0500

    edits to css and php

--- gui.php
+++ gui.php
@@ -8,23 +8,19 @@
     <link rel="stylesheet" href="gui.css">
     <link href="https://fonts.googleapis.com/css2?family=Assistant:wght@200..800&display=swap" rel="stylesheet">
     <meta charset="utf-8">
-    <title>CPS510: Patient Clinic DB</title>
+    <title>PatientLink</title>
 </head>
 
 <body>
     <header>
-        <h1>
-            Patient Clinic Database
-        </h1>
+        <h1>PatientLink</h1>
     </header>
+    <hr>
 
     <main>
-
        <section>
             <h3>idk</h3>
         </section>
-        <hr>
-
     </main>
 </body>
 </html>
