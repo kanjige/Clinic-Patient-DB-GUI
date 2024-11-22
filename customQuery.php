7cca0c591bd31b769bfef7065b7864b582302f0f
Mikayla Morrison <115184215+mikaylamorrison@users.noreply.github.com>
2024-11-22 01:27:58 -0500

    got rid of styling

--- customQuery.php
+++ customQuery.php
@@ -1,30 +1,5 @@
 <?php include('login.php'); ?>
 
-<style>
-        table {
-                border: 1px solid;
-                border-collapse: collapse;
-                text-align: center;
-                margin-left: auto;
-                margin-right: auto;
-        }
-
-        td,
-        th {
-                padding: 15px;
-                border: 1px solid;
-        }
-
-        tr:nth-child(even) {
-                background-color: #f2f2f2;
-        }
-
-        th {
-                background-color: #8d1fc0;
-                color: white;
-        }
-</style>
-
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $customQuery = $_POST['customQuery'];
