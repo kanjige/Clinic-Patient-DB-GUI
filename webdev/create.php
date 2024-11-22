3d099fd1aa394428a86fd60342c6d30f00938238
Eric Wong <122705933+kanjige@users.noreply.github.com>
2024-11-21 23:55:48 -0500

    Update create.php

--- webdev/create.php
+++ webdev/create.php
@@ -4,14 +4,16 @@
 
 <?php
 
-$allergies_create = "CREATE TABLE ALLERGIES (	
+
+$tables = [
+"ALLERGIES" => "CREATE TABLE ALLERGIES (	
     ALLERGIESID CHAR(10 BYTE), 
 	TYPE VARCHAR2(255 BYTE), 
 	SEVERITY VARCHAR2(50 BYTE), 
 	ADDITIONALCOMMENTS VARCHAR2(255 BYTE)
-   );";
+   )",
 
-$appointment_info_create = "CREATE TABLE APPOINTMENT_INFORMATION(	
+"APPOINTMENT_INFORMATION" => "CREATE TABLE APPOINTMENT_INFORMATION(	
     APPOINTMENT_ID CHAR(10 BYTE),
 	PATIENTID CHAR(16 BYTE),
 	DATEOFAPPOINTMENT DATE,
@@ -19,50 +21,50 @@ $appointment_info_create = "CREATE TABLE APPOINTMENT_INFORMATION(
 	PHYSICIAN VARCHAR2(50 BYTE),
 	DIAGNOSIS VARCHAR2(255 BYTE),
 	TREATMENTPLAN VARCHAR2(255 BYTE)
-   );";
+   )",
 
-$billing_history_create = "CREATE TABLE BILLING_HISTORY(	
+"BILLING_HISTORY" => "CREATE TABLE BILLING_HISTORY(	
     BILLSID CHAR(10 BYTE),
 	CHARGENAME VARCHAR2(50 BYTE),
 	DATEOFCHARGE DATE,
 	ORIGINALCHARGE NUMBER(7,2),
 	OUTSTANDINGPAYMENT NUMBER(7,2),
 	PAID NUMBER(7,2)
-   );";
+   )",
 
-$billing_info_create = "CREATE TABLE BILLING_INFORMATION(	
+"BILLING_INFORMATION" => "CREATE TABLE BILLING_INFORMATION(	
     BILLINGINFORMATIONID CHAR(10 BYTE),
     ADDRESS VARCHAR2(255 BYTE),
     CARDNUMBER CHAR(16 BYTE),
     BILLSID CHAR(10 BYTE),
     INSURANCECOVERAGEID CHAR(10 BYTE)
-    );";
+    )",
 
-$emergency_contact_create = "CREATE TABLE EMERGENCY_CONTACT(	
+"EMERGENCY_CONTACT" => "CREATE TABLE EMERGENCY_CONTACT(	
     EMERGENCYCONTACTSID CHAR(10 BYTE), 
 	FULLNAME VARCHAR2(50 BYTE), 
 	PHONENUMBER NUMBER, 
 	EMAIL VARCHAR2(50 BYTE), 
 	SECONDARYPHONENUMBER NUMBER
-   );";
+   )",
 
-$emergency_relations_create = "CREATE TABLE EMERGENCY_RELATIONS(	
+"EMERGENCY_RELATIONS" => "CREATE TABLE EMERGENCY_RELATIONS(	
     EMERGENCYCONTACTSID CHAR(10 BYTE), 
 	PATIENTID CHAR(16 BYTE), 
 	RELATION VARCHAR2(50 BYTE), 
 	PRIORITY VARCHAR2(3 BYTE)
-   );";
+   )",
 
-$immunization_info_create = "CREATE TABLE INSURANCE_INFORMATION(	
+"INSURANCE_INFORMATION" => "CREATE TABLE INSURANCE_INFORMATION(	
     INSURANCECOVERAGEID CHAR(10 BYTE),
 	INSURANCEPROVIDER VARCHAR2(50 BYTE),
 	POLICYNUMBER VARCHAR2(20 BYTE),
 	COVERAGETYPE VARCHAR2(100 BYTE),
 	CURRENTCOVERAGE NUMBER
-   );";
+   )",
 
 
-$medical_history_create = "CREATE TABLE MEDICAL_HISTORY(	
+"MEDICAL_HISTORY" => "CREATE TABLE MEDICAL_HISTORY(	
     PATIENTID CHAR(16 BYTE),
 	ALLERGIESID CHAR(10 BYTE),
 	FAMILYHISTORY VARCHAR2(255 BYTE),
@@ -70,24 +72,24 @@ $medical_history_create = "CREATE TABLE MEDICAL_HISTORY(
 	PRESCRIPTIONSID CHAR(10 BYTE),
 	IMMUNIZATIONSID CHAR(10 BYTE),
 	COMMENTS VARCHAR2(255 BYTE)
-   );";
+   )",
 
-$medical_staff_create = "CREATE TABLE MEDICAL_STAFF(	
+"MEDICAL_STAFF" => "CREATE TABLE MEDICAL_STAFF(	
     ID CHAR(16 BYTE),
 	FULLNAME VARCHAR2(50 BYTE),
 	ROLE VARCHAR2(50 BYTE)
-   );";
+   )",
 
    
-$patient_contact_info_create =  "CREATE TABLE PATIENT_CONTACT_INFORMATION(	
+"PATIENT_CONTACT_INFORMATION" => "CREATE TABLE PATIENT_CONTACT_INFORMATION(	
     PATIENTID CHAR(16 BYTE),
     ADDRESS VARCHAR2(255 BYTE),
     EMAIL VARCHAR2(50 BYTE),
     PHONENUMBER NUMBER
-);";
+)",
 
 
-$patient_info_create =  "CREATE TABLE PATIENT_INFORMATION(	
+"PATIENT_INFORMATION" => "CREATE TABLE PATIENT_INFORMATION(	
     PATIENTID CHAR(16 BYTE),
 	FULLNAME VARCHAR2(50 BYTE),
 	HEALTHCARDNUMBER CHAR(10 BYTE),
@@ -98,10 +100,10 @@ $patient_info_create =  "CREATE TABLE PATIENT_INFORMATION(
 	BLOODTYPE VARCHAR2(5 BYTE),
 	BILLINGINFORMATIONID CHAR(10 BYTE),
 	EMERGENCYCONTACTSID CHAR(10 BYTE)
-);";
+)",
 
 
-$prescriptions_medication_create =  "CREATE TABLE PRESCRIPTIONS_AND_MEDICATION(	
+"PRESCRIPTIONS_AND_MEDICATION" => "CREATE TABLE PRESCRIPTIONS_AND_MEDICATION(	
     PRESCRIPTIONSID CHAR(10 BYTE),
 	CURRENTLYPRESCRIBED NUMBER(1,0) DEFAULT 0,
 	MEDICATIONID CHAR(8 BYTE),
@@ -111,9 +113,9 @@ $prescriptions_medication_create =  "CREATE TABLE PRESCRIPTIONS_AND_MEDICATION(
 	DATEPRESCRIBED DATE,
 	REFILLS_REMAINING NUMBER,
 	PRESCRIBINGPHYSICIAN VARCHAR2(50 BYTE)
-);";
+)",
 
-$previous_operations_create =  "CREATE TABLE PREVIOUS_OPERATIONS(	
+"PREVIOUS_OPERATIONS" => "CREATE TABLE PREVIOUS_OPERATIONS(	
     PREVIOUSOPERATIONSID CHAR(10 BYTE),
 	OPERATIONDATE DATE,
 	SURGERYTYPE VARCHAR2(50 BYTE),
@@ -123,90 +125,23 @@ $previous_operations_create =  "CREATE TABLE PREVIOUS_OPERATIONS(
 	ATTENDINGPHYSICIAN VARCHAR2(50 BYTE),
 	COMPLICATIONS VARCHAR2(255 BYTE),
 	NOTES VARCHAR2(255 BYTE)
-);";
+)"
+];
 
 
+foreach ($tables as $tablename => $query) {
+    $stid = oci_parse($conn, $query);
+    if (oci_execute($stid)) {
+        echo "<div>Table '$tablename' Successfully Created.</div>";
+    } else {
+        $error = oci_error($stid);
+        echo "<div>Error Creating Table '$tablename': " . htmlentities($error['message']) . "</div>";
+    }
 
-
-
-
-
-if (mysqli_query($conn, $allergies_create)) {
-    echo "Table 'Allergies' Successfully Created.<br>";
-} else {
-    echo "Error Table: " . $allergies_create . "<br>" . mysqli_error($conn);
+    oci_free_statement($stid);
 }
 
-if (mysqli_query($conn, $appointment_info_create)) {
-    echo "Table 'Appointment Information' Successfully Created.<br>";
-} else {
-    echo "Error Table: " . $appointment_info_create . "<br>" . mysqli_error($conn);
-}
-
-if (mysqli_query($conn, $billing_history_create)) {
-    echo "Table 'Billing History' Successfully Created.<br>";
-} else {
-    echo "Error Table: " . $billing_history_create . "<br>" . mysqli_error($conn);
-}
-
-if (mysqli_query($conn, $billing_info_create)) {
-    echo "Table 'Billing Information' Successfully Created.<br>";
-} else {
-    echo "Error Table: " . $billing_info_create . "<br>" . mysqli_error($conn);
-}
-
-if (mysqli_query($conn, $emergency_contact_create)) {
-    echo "Table 'Emergency Contact' Successfully Created.<br>";
-} else {
-    echo "Error Table: " . $emergency_contact_create . "<br>" . mysqli_error($conn);
-}
-
-if (mysqli_query($conn, $emergency_relations_create)) {
-    echo "Table 'Emergency Relations' Successfully Created.<br>";
-} else {
-    echo "Error Table: " . $emergency_relations_create . "<br>" . mysqli_error($conn);
-}
-
-if (mysqli_query($conn, $immunization_info_create)) {
-    echo "Table 'Immunization Information' Successfully Created.<br>";
-} else {
-    echo "Error Table: " . $immunization_info_create . "<br>" . mysqli_error($conn);
-}
-
-if (mysqli_query($conn, $medical_history_create)) {
-    echo "Table 'Medical History' Successfully Created.<br>";
-} else {
-    echo "Error Table: " . $medical_history_create . "<br>" . mysqli_error($conn);
-}
-
-if (mysqli_query($conn, $medical_staff_create)) {
-    echo "Table 'Medical' Successfully Created.<br>";
-} else {
-    echo "Error Table: " . $medical_staff_create . "<br>" . mysqli_error($conn);
-}
-
-if (mysqli_query($conn, $patient_contact_info_create)) {
-    echo "Table 'Patient Contact Information' Successfully Created.<br>";
-} else {
-    echo "Error Table: " . $patient_contact_info_create . "<br>" . mysqli_error($conn);
-}
-
-if (mysqli_query($conn, $prescriptions_medication_create)) {
-    echo "Table 'Prescriptions and Medication' Successfully Created.<br>";
-} else {
-    echo "Error Table: " . $prescriptions_medication_create . "<br>" . mysqli_error($conn);
-}
-
-if (mysqli_query($conn, $previous_operations_create)) {
-    echo "Table 'Previous Operations' Successfully Created.<br>";
-} else {
-    echo "Error Table: " . $previous_operations_create . "<br>" . mysqli_error($conn);
-}
-
-
-
-
-mysqli_close($conn);
+oci_close($conn);
 
 ?>
 
