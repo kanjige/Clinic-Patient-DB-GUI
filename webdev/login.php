fe1d14c11b8033634bc7d67777a035b57c0cb38b
RohanManoharan <rohan.manoharan@ryerson.ca>
2024-11-21 21:46:24 -0500

    Update login.php

--- webdev/login.php
+++ webdev/login.php
@@ -2,7 +2,7 @@
     error_reporting(E_ALL);
     ini_set('display_errors', 'On');
     // Create connection to Oracle
-    $conn = oci_connect('username', 'password','connection');
+    $conn = oci_connect('rpmanoha', '04309408','(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))');
     if (!$conn) {
         $m = oci_error();
         echo $m['message'];
